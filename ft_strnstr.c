8c58321eebc2f1e7e788d56c49143c82a7841da7
lanximaomao <linlin.sun@outlook.com>
2022-10-31 20:49:01 +0200

    new strnstr function

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,26 +14,36 @@
 
 char * ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
+	// const char *first;
 
 	i = 0;
 	j = 0;
-	if (ft_strlen(needle) > ft_strlen(haystack) || ft_strlen(needle) > len)
+	// first = haystack;
+	if (ft_strlen((char*)needle) > ft_strlen((char*)haystack) || ft_strlen((char*)needle) > (int)len)
 		return(0);
 	if(needle == '\0')
-		return(haystack);
-	while (i < ft_strlen(haystack) && i < len)
+		return((char*)haystack);
+	while (i < ft_strlen((char*)haystack) && i < (int)len && j < ft_strlen((char*)needle))
 	{
-		while (haystack[i] == needle[j])
-		{
-
-		}
-
-
+		if (haystack[i] == needle[j])
+			j++;
+		else
+			j = 0;
 		i++;
 	}
-	needle[i]
-
+	if(j == ft_strlen((char*)needle))
+		return((char*)haystack + j);
+	else
+		return(0);
 }
 
+// int	main()
+// {
+// 	char s1[] = "hello";
+// 	char s2[] = "ll";
+// 	printf("%s\n", ft_strnstr(s1, s2, 5));
+// 	printf("%s\n", strnstr(s1, s2, 5));
+// 	return(0);
+// }
