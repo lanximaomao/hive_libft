f8a1668b2991975e05381e8b67504f5ace233b25
Lin <lsun@student.hive.fi>
2022-11-01 17:06:14 +0200

    updated ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 09:32:27 by lsun              #+#    #+#             */
-/*   Updated: 2022/11/01 13:58:30 by lsun             ###   ########.fr       */
+/*   Updated: 2022/11/01 17:04:22 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && s1[i] == s2[i] && s1[i] && s2[i])
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return (s1[i] - s2[i]);
 }
