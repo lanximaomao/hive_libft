dd371f8d5226ec214d4a21d5f7fb5ac76ae3ed92
Lin <lsun@student.hive.fi>
2022-11-10 17:22:45 +0200

    fixed strtrim norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,26 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:14:55 by lsun              #+#    #+#             */
-/*   Updated: 2022/11/10 17:13:33 by lsun             ###   ########.fr       */
+/*   Updated: 2022/11/10 17:22:30 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_trim_assign(char const *s1, int start, int end)
+{
+	char	*strim;
+	int		i;
+
+	i = 0;
+	strim = (char *)malloc(sizeof(char) * (end - start + 2));
+	if (!strim)
+		return (NULL);
+	while (start <= end)
+		strim[i++] = s1[start++];
+	strim[i] = '\0';
+	return (strim);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -18,7 +33,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		check;
 	int		start;
 	int		end;
-	char	*strim;
 
 	i = 0;
 	start = 0;
@@ -34,12 +48,5 @@ char	*ft_strtrim(char const *s1, char const *set)
 			end--;
 		i++;
 	}
-	strim = (char *)malloc(sizeof(char) * (end - start + 2));
-	if (!strim)
-		return (NULL);
-	i = 0;
-	while (start <= end)
-		strim[i++] = s1[start++];
-	strim[i] = '\0';
-	return (strim);
+	return (ft_trim_assign(s1, start, end));
 }
