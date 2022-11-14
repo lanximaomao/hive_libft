9e006028c91ff8a218b1d32a3dd0aa67093b0d7b
Lin <lsun@student.hive.fi>
2022-11-14 21:06:00 +0200

    two more

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,27 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/11 12:15:59 by linlinsun         #+#    #+#             */
-/*   Updated: 2022/11/14 21:00:56 by lsun             ###   ########.fr       */
+/*   Created: 2022/11/11 12:16:04 by linlinsun         #+#    #+#             */
+/*   Updated: 2022/11/14 13:38:37 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*temp;
+	int		check;
+	t_list	*m;
+	t_list	*n;
 
-	if (!lst)
-		return ;
-	temp = lst;
-	while (temp->next)
+	m = ft_lstnew(f(lst));
+	n = ft_lstnew(f(lst));
+	check = -1;
+	if (lst && lst->next)
+		ft_lstmap(lst->next, f, del);
+	if (check < 0)
 	{
-		f(temp);
-		temp = temp->next;
+		m = ft_lstnew(f(lst));
+		m->next = NULL;
+		check = 1;
 	}
+	else
+	{
+		n = ft_lstnew(f(lst));
+		n->next = m;
+	}
+	return (n);
 }
