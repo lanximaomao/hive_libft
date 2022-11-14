06b036903675f13017509fcec488f9d8350f6f9f
Lin <lsun@student.hive.fi>
2022-11-14 22:24:54 +0200

    fixed

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:16:04 by linlinsun         #+#    #+#             */
-/*   Updated: 2022/11/14 21:44:28 by lsun             ###   ########.fr       */
+/*   Updated: 2022/11/14 22:23:23 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**new_lst;
+	t_list	*new_lst;
 	t_list	*new_node;
 
+	if (lst)
+	{
+		new_lst = ft_lstnew(f(lst->content));
+		if (!new_lst)
+			return (0);
+		lst = lst->next;
+	}
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
-		if (!new_lst)
-			del(new_lst->content);
-		new_lst = ft_isadd_back(new_lst, new_node);
+		if (!new_node)
+		{
+			del(new_node->content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
