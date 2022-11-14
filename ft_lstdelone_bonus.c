24b1e1c1d865ba1e740a4448db637787416eba73
Lin <lsun@student.hive.fi>
2022-11-14 20:49:22 +0200

    lstdelone

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:15:57 by linlinsun         #+#    #+#             */
-/*   Updated: 2022/11/14 19:54:06 by lsun             ###   ########.fr       */
+/*   Updated: 2022/11/14 20:48:18 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	t_list	*temp;
 
+	if (!lst || !del)
+		return ;
 	temp = lst;
 	del(temp->content);
 	free(temp);
