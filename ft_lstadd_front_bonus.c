05c3afc4c849370b1c7cdb7fbb8e0600bbac5e5f
Lin <lsun@student.hive.fi>
2022-11-14 20:59:37 +0200

    add front

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: lsun <lsun@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:15:50 by linlinsun         #+#    #+#             */
-/*   Updated: 2022/11/14 20:35:11 by lsun             ###   ########.fr       */
+/*   Updated: 2022/11/14 20:58:13 by lsun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!*lst)
-		new = *lst;
+	if (!lst)
+		return ;
 	new->next = *lst;
-	new = *lst;
+	*lst = new;
 }
